4ed8616eafe290359c25f013cb115e0152079905
Dayman267 <108380150+Dayman267@users.noreply.github.com>
2024-03-12 03:14:24 +0200

    Lighter is flashing now, assets added, working on terrain

---
+++ Assets/Scripts/Flashing.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+
+public class Flashing : MonoBehaviour
+{
+    [Range(0f, 10f)] public float intensity;
+    [Range(0f, 10f)] public float seconds;
+    [Range(1f, 50f)] public float upperLimit;
+    [Range(0f, 5f)] public float lowerLimit;
+
+    private void Start() => StartCoroutine(FlashingCoroutine());
+
+    private IEnumerator FlashingCoroutine()
+    {
+        Light light = GetComponent<Light>();
+        while (true)
+        {
+            float localIntesity = light.intensity + Random.Range(-intensity, intensity);
+            if(localIntesity > upperLimit) light.intensity = upperLimit;
+            else if (localIntesity < lowerLimit) light.intensity = lowerLimit;
+            else light.intensity = localIntesity;
+            
+            yield return new WaitForSeconds(Random.Range(-seconds, seconds));
+        }
+    }
+}
