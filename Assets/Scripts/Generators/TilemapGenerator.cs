aa60e39fa6f3837e82f95b9e1e1e3b768998bd5d
Dayman267 <108380150+Dayman267@users.noreply.github.com>
2024-04-14 00:01:48 +0300

    Emergency Commit, while russians bombs us

--- Assets/Scripts/Generators/TilemapGenerator.cs
+++ Assets/Scripts/Generators/TilemapGenerator.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
+using System.Reflection;
 
 public class TilemapGenerator : MonoBehaviour
 {
@@ -7,9 +11,11 @@ public class TilemapGenerator : MonoBehaviour
     public GameObject[] grayTerrains;
     public GameObject[] greenTerrains;
     public GameObject[] yellowTerrains;
-    private GameObject[][] terrainsArrays;
+    private List<GameObject[]> terrainsArrays = new();
+    private List<GameObject> pickedTerrains1 = new();
     private GameObject[] pickedTerrains;
-    //private System.Random random = new System.Random();
+
+    private TerrainGenerator terrainGenerator;
     
     public int[,] gameObjectsToSpawn;
     
@@ -30,18 +36,50 @@ public class TilemapGenerator : MonoBehaviour
 
     public void Start()
     {
-        terrainsArrays = new [] {grayTerrains, greenTerrains, yellowTerrains };
-        int randomIndex = Random.Range(0, terrainsArrays.Length-1);
-        pickedTerrains = terrainsArrays[randomIndex];
-        for (int i = 0; i < pickedTerrains.Length; i++)
-        {
-            
-        }
-        objectSize = (int)pickedTerrains[0].GetComponent<Terrain>().terrainData.size.x;
+        CreateTerrainsArrays();
+        PickTerrainsAndCreateVersions();
+        objectSize = (int)pickedTerrains1[0].GetComponent<Terrain>().terrainData.size.x;
         gameObjectsToSpawn = new int[width, height];
         SpawnFirstNineObjects();
     }
-    
+
+    private void CreateTerrainsArrays()
+    {
+        FieldInfo[] fields = typeof(TilemapGenerator)
+            .GetFields(BindingFlags.Instance | BindingFlags.Public);
+        
+        foreach (FieldInfo field in fields)
+        {
+            if (field.FieldType == typeof(GameObject[]))
+            {
+                terrainsArrays.Add((GameObject[])field.GetValue(this));
+            }
+        }
+    }
+
+    private void PickTerrainsAndCreateVersions()
+    {
+        terrainGenerator = GetComponent<TerrainGenerator>();
+        int randomIndex = UnityEngine.Random.Range(0, terrainsArrays.Count);
+        
+        for (int i = 0, j = 0; i < terrainsArrays[randomIndex].Length; i++, j++)
+        {
+            pickedTerrains1.Add(terrainsArrays[randomIndex][i]);
+            pickedTerrains1.Add(terrainGenerator.GenerateMirroredTerrain(pickedTerrains1[j]));
+            j++;
+            Destroy(pickedTerrains1[j]);
+            for (int k = 0; k < 3; k++)
+            {
+                pickedTerrains1.Add(terrainGenerator.GenerateRotatedTerrain(pickedTerrains1[j]));
+                j++;
+                Destroy(pickedTerrains1[j]);
+                pickedTerrains1.Add(terrainGenerator.GenerateMirroredTerrain(pickedTerrains1[j]));
+                j++;
+                Destroy(pickedTerrains1[j]);
+            }
+        }
+    }
+
     private void SpawnFirstNineObjects()
     {
         for (int x = 0; x < width; x++)
@@ -65,35 +103,35 @@ public class TilemapGenerator : MonoBehaviour
                 
                 if (x > 0 && gameObjectsToSpawn[x - 1,y] == -1)
                 {
-                    gameObjectsToSpawn[x - 1,y] = Random.Range(0, pickedTerrains.Length);
+                    gameObjectsToSpawn[x - 1,y] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
                 }
                 if (y > 0 && gameObjectsToSpawn[x,y - 1] == -1)
                 {
-                    gameObjectsToSpawn[x,y - 1] = Random.Range(0, pickedTerrains.Length);
+                    gameObjectsToSpawn[x,y - 1] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
                 }
                 if (x > 0 && y > 0 && gameObjectsToSpawn[x - 1,y - 1] == -1) 
                 {
-                    gameObjectsToSpawn[x - 1,y - 1] = Random.Range(0, pickedTerrains.Length);
+                    gameObjectsToSpawn[x - 1,y - 1] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
                 }
                 if (x < maxX && gameObjectsToSpawn[x + 1,y] == -1) 
                 {
-                    gameObjectsToSpawn[x + 1,y] = Random.Range(0, pickedTerrains.Length);
+                    gameObjectsToSpawn[x + 1,y] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
                 }
                 if (y < maxY && gameObjectsToSpawn[x,y + 1] == -1) 
                 {
-                    gameObjectsToSpawn[x,y + 1] = Random.Range(0, pickedTerrains.Length);
+                    gameObjectsToSpawn[x,y + 1] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
                 }
                 if (x < maxX && y < maxY && gameObjectsToSpawn[x + 1,y + 1] == -1) 
                 {
-                    gameObjectsToSpawn[x + 1,y + 1] = Random.Range(0, pickedTerrains.Length);
+                    gameObjectsToSpawn[x + 1,y + 1] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
                 }
                 if (x > 0 && y < maxY && gameObjectsToSpawn[x - 1,y + 1] == -1) 
                 {
-                    gameObjectsToSpawn[x - 1,y + 1] = Random.Range(0, pickedTerrains.Length);
+                    gameObjectsToSpawn[x - 1,y + 1] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
                 }
                 if (y > 0 && x < maxX && gameObjectsToSpawn[x + 1,y - 1] == -1)
                 {
-                    gameObjectsToSpawn[x + 1,y - 1] = Random.Range(0, pickedTerrains.Length);
+                    gameObjectsToSpawn[x + 1,y - 1] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
                 }
                 isFinished = true;
             }
@@ -104,11 +142,13 @@ public class TilemapGenerator : MonoBehaviour
             for (int y = 0; y < height; y++)
             {
                 if (gameObjectsToSpawn[x, y] == -1) continue;
-                Instantiate(
-                    pickedTerrains[gameObjectsToSpawn[x, y]],
+                GameObject instance = Instantiate(
+                    pickedTerrains1[gameObjectsToSpawn[x, y]],
                     new Vector3(x-width/2, 0, y-height/2) * objectSize, 
                     Quaternion.identity, 
                     parentToSet.transform);
+                instance.GetComponent<Terrain>().enabled = true;
+                instance.GetComponent<ObjectsGenerator>().enabled = true;
             }
         }
     }
@@ -117,65 +157,79 @@ public class TilemapGenerator : MonoBehaviour
     {
         float positionX = (position.x / objectSize) + width/2;
         float positionY = (position.z / objectSize) + height/2;
-    
         int maxX = gameObjectsToSpawn.GetLength(0) - 1;
         int maxY = gameObjectsToSpawn.GetLength(1) - 1;
-    
         if (positionX > 0 && gameObjectsToSpawn[(int)positionX - 1, (int)positionY] == -1)
         {
-            gameObjectsToSpawn[(int)positionX - 1,(int)positionY] = Random.Range(0, pickedTerrains.Length);
-            Instantiate(pickedTerrains[gameObjectsToSpawn[(int)positionX - 1,(int)positionY]],
+            gameObjectsToSpawn[(int)positionX - 1,(int)positionY] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
+            GameObject instance = Instantiate(pickedTerrains1[gameObjectsToSpawn[(int)positionX - 1,(int)positionY]],
                 new Vector3((int)positionX - 1-width/2, 0, (int)positionY-height/2) * objectSize,
                 Quaternion.identity, parentToSet.transform);
+                instance.GetComponent<Terrain>().enabled = true;
+                instance.GetComponent<ObjectsGenerator>().enabled = true;
         }
         if (positionY > 0 && gameObjectsToSpawn[(int)positionX, (int)positionY - 1] == -1)
         {
-            gameObjectsToSpawn[(int)positionX, (int)positionY - 1] = Random.Range(0, pickedTerrains.Length);
-            Instantiate(pickedTerrains[gameObjectsToSpawn[(int)positionX,(int)positionY - 1]],
+            gameObjectsToSpawn[(int)positionX, (int)positionY - 1] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
+            GameObject instance = Instantiate(pickedTerrains1[gameObjectsToSpawn[(int)positionX,(int)positionY - 1]],
                 new Vector3((int)positionX -width/2, 0, (int)positionY - 1-height/2) * objectSize,
                 Quaternion.identity, parentToSet.transform);
+                instance.GetComponent<Terrain>().enabled = true;
+                instance.GetComponent<ObjectsGenerator>().enabled = true;
         }
         if (positionX > 0 && positionY > 0 && gameObjectsToSpawn[(int)positionX - 1, (int)positionY - 1] == -1) 
         {
-            gameObjectsToSpawn[(int)positionX - 1, (int)positionY - 1] = Random.Range(0, pickedTerrains.Length);
-            Instantiate(pickedTerrains[gameObjectsToSpawn[(int)positionX - 1,(int)positionY -1]],
+            gameObjectsToSpawn[(int)positionX - 1, (int)positionY - 1] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
+            GameObject instance = Instantiate(pickedTerrains1[gameObjectsToSpawn[(int)positionX - 1,(int)positionY -1]],
                 new Vector3((int)positionX - 1-width/2, 0, (int)positionY-1-height/2) * objectSize,
                 Quaternion.identity, parentToSet.transform);
+                instance.GetComponent<Terrain>().enabled = true;
+                instance.GetComponent<ObjectsGenerator>().enabled = true;
         }
         if (positionX < maxX && gameObjectsToSpawn[(int)positionX + 1, (int)positionY] == -1) 
         {
-            gameObjectsToSpawn[(int)positionX + 1, (int)positionY] = Random.Range(0, pickedTerrains.Length);
-            Instantiate(pickedTerrains[gameObjectsToSpawn[(int)positionX + 1,(int)positionY]],
+            gameObjectsToSpawn[(int)positionX + 1, (int)positionY] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
+            GameObject instance = Instantiate(pickedTerrains1[gameObjectsToSpawn[(int)positionX + 1,(int)positionY]],
                 new Vector3((int)positionX + 1-width/2, 0, (int)positionY-height/2) * objectSize,
                 Quaternion.identity, parentToSet.transform);
+                instance.GetComponent<Terrain>().enabled = true;
+                instance.GetComponent<ObjectsGenerator>().enabled = true;
         }
         if (positionY < maxY && gameObjectsToSpawn[(int)positionX, (int)positionY + 1] == -1) 
         {
-            gameObjectsToSpawn[(int)positionX, (int)positionY + 1] = Random.Range(0, pickedTerrains.Length);
-            Instantiate(pickedTerrains[gameObjectsToSpawn[(int)positionX,(int)positionY+1]],
+            gameObjectsToSpawn[(int)positionX, (int)positionY + 1] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
+            GameObject instance = Instantiate(pickedTerrains1[gameObjectsToSpawn[(int)positionX,(int)positionY+1]],
                 new Vector3((int)positionX-width/2, 0, (int)positionY+1-height/2) * objectSize,
                 Quaternion.identity, parentToSet.transform);
+                instance.GetComponent<Terrain>().enabled = true;
+                instance.GetComponent<ObjectsGenerator>().enabled = true;
         }
         if (positionX < maxX && positionY < maxY && gameObjectsToSpawn[(int)positionX + 1, (int)positionY + 1] == -1) 
         {
-            gameObjectsToSpawn[(int)positionX + 1, (int)positionY + 1] = Random.Range(0, pickedTerrains.Length);
-            Instantiate(pickedTerrains[gameObjectsToSpawn[(int)positionX + 1,(int)positionY+1]],
+            gameObjectsToSpawn[(int)positionX + 1, (int)positionY + 1] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
+            GameObject instance = Instantiate(pickedTerrains1[gameObjectsToSpawn[(int)positionX + 1,(int)positionY+1]],
                 new Vector3((int)positionX + 1-width/2, 0, (int)positionY+1-height/2) * objectSize,
                 Quaternion.identity, parentToSet.transform);
+                instance.GetComponent<Terrain>().enabled = true;
+                instance.GetComponent<ObjectsGenerator>().enabled = true;
         }
         if (positionX > 0 && positionY < maxY && gameObjectsToSpawn[(int)positionX - 1, (int)positionY + 1] == -1) 
         {
-            gameObjectsToSpawn[(int)positionX - 1, (int)positionY + 1] = Random.Range(0, pickedTerrains.Length);
-            Instantiate(pickedTerrains[gameObjectsToSpawn[(int)positionX - 1,(int)positionY+1]],
+            gameObjectsToSpawn[(int)positionX - 1, (int)positionY + 1] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
+            GameObject instance = Instantiate(pickedTerrains1[gameObjectsToSpawn[(int)positionX - 1,(int)positionY+1]],
                 new Vector3((int)positionX - 1-width/2, 0, (int)positionY+1-height/2) * objectSize,
                 Quaternion.identity, parentToSet.transform);
+                instance.GetComponent<Terrain>().enabled = true;
+                instance.GetComponent<ObjectsGenerator>().enabled = true;
         }
         if (positionY > 0 && positionX < maxX && gameObjectsToSpawn[(int)positionX + 1, (int)positionY - 1] == -1) 
         {
-            gameObjectsToSpawn[(int)positionX + 1, (int)positionY - 1] = Random.Range(0, pickedTerrains.Length);
-            Instantiate(pickedTerrains[gameObjectsToSpawn[(int)positionX + 1,(int)positionY-1]],
+            gameObjectsToSpawn[(int)positionX + 1, (int)positionY - 1] = UnityEngine.Random.Range(0, pickedTerrains1.Count);
+            GameObject instance = Instantiate(pickedTerrains1[gameObjectsToSpawn[(int)positionX + 1,(int)positionY-1]],
                 new Vector3((int)positionX + 1-width/2, 0, (int)positionY-1-height/2) * objectSize,
                 Quaternion.identity, parentToSet.transform);
+                instance.GetComponent<Terrain>().enabled = true;
+                instance.GetComponent<ObjectsGenerator>().enabled = true;
         }
     }
 }
